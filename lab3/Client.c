bf2506211223eb22db2a099ef78bbc9443db1bd4
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-12-19 21:48:51 +0530

    client

--- lab3/Client.c
+++ lab3/Client.c
@@ -1,82 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <time.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-#define SIZE 256
-int PORT;
+#define PORT 4444
+#define BUF_SIZE 2000
 
-void time_stamp(char *actual)
+int main(int argc, char **argv)
 {
-    bzero((char *)actual, SIZE);
-    char *timestamp = (char *)malloc(sizeof(char) * SIZE);
-    time_t ltime;
-    ltime = time(NULL);
-    struct tm *tm;
-    tm = localtime(&ltime);
+    struct sockaddr_in addr, cl_addr;
+    int sockfd, ret;
+    char buffer[BUF_SIZE];
+    struct hostent *server;
+    char *serverAddr;
 
-    sprintf(timestamp, "%02d/%02d/%04d, %02d:%02d:%02d", tm->tm_mday, tm->tm_mon + 1,
-            tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);
-    strcpy(actual, timestamp);
-    bzero((char *)timestamp, SIZE);
-}
-
-int main()
-{
-    int clientSocket;
-    struct sockaddr_in server_address;
-    char msg[SIZE], buf[SIZE], currTime[SIZE];
-
-    printf("\nEnter the port number you want to bind to: ");
-    scanf("%d", &PORT);
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0)
+    if (argc < 2)
     {
-        perror("Can't create Socket..\n");
-        return 1;
+        printf("usage: client < ip address >\n");
+        exit(1);
     }
 
-    printf("Client socket created..\n");
-    memset(&server_address, '\0', sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    int c = connect(clientSocket, (struct sockaddr *)&server_address, sizeof(server_address));
+    serverAddr = argv[1];
 
-    if (c < 0)
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
-        perror("Can't connect to the server..\n");
-        return 1;
+        printf("Error creating socket!\n");
+        exit(1);
     }
+    printf("Socket created...\n");
 
-    time_stamp(currTime);
-    printf("%s\nConnected to the server..\n", currTime);
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(serverAddr);
+    addr.sin_port = PORT;
 
-    while (1)
+    ret = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret < 0)
     {
-        printf("Enter Client: ");
-        fgets(buf, SIZE - 1, stdin);
-        send(clientSocket, buf, sizeof(buf), 0);
-        if (strcmp(buf, "exit\n") == 0)
-        {
-            close(clientSocket);
-            time_stamp(currTime);
-            printf("%s\nDisconnected from the server..\n", currTime);
-            return 1;
-        }
-        if (recv(clientSocket, msg, sizeof(msg), 0) < 0)
-        {
-            perror("Can't receive message..\n");
-            return 1;
-        }
+        printf("Error connecting to the server!\n");
+        exit(1);
+    }
+    printf("Connected to the server...\n");
 
-        printf("Server: %s", msg);
-        bzero(msg, sizeof(msg));
+    memset(buffer, 0, BUF_SIZE);
+    printf("Enter your message(s): ");
+
+    while (fgets(buffer, BUF_SIZE, stdin) != NULL)
+    {
+        ret = sendto(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&addr, sizeof(addr));
+        if (ret < 0)
+        {
+            printf("Error sending data!\n\t-%s", buffer);
+        }
+        ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
+        if (ret < 0)
+        {
+            printf("Error receiving data!\n");
+        }
+        else
+        {
+            printf("Received: ");
+            fputs(buffer, stdout);
+            printf("\n");
+        }
     }
 
     return 0;
--- lab3/Server.c
+++ lab3/Server.c
@@ -1,110 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
-#include <time.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
-#define SIZE 256
-int PORT;
+#define PORT 4444
+#define BUF_SIZE 2000
+#define CLADDR_LEN 100
 
-void time_stamp(char *actual)
+void main()
 {
-    bzero((char *)actual, SIZE);
-    char *timestamp = (char *)malloc(sizeof(char) * SIZE);
-    time_t ltime;
-    ltime = time(NULL);
-    struct tm *tm;
-    tm = localtime(&ltime);
-
-    sprintf(timestamp, "%02d/%02d/%04d, %02d:%02d:%02d", tm->tm_mday, tm->tm_mon + 1,
-            tm->tm_year + 1900, tm->tm_hour, tm->tm_min, tm->tm_sec);
-    strcpy(actual, timestamp);
-    bzero((char *)timestamp, SIZE);
-}
-
-int main()
-{
-    int serverSocket, newSocket;
-    struct sockaddr_in server_address, new_address;
+    struct sockaddr_in addr, cl_addr;
+    int sockfd, len, ret, newsockfd;
+    char buffer[BUF_SIZE];
     pid_t childpid;
+    char clientAddr[CLADDR_LEN];
 
-    socklen_t serverLen = sizeof(server_address);
-    socklen_t clientLen = sizeof(new_address);
-    char msg[SIZE], buf[SIZE], currTime[SIZE];
-
-    printf("\nEnter the port number you want to bind to: ");
-    scanf("%d", &PORT);
-
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0)
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
-        perror("Can't create Socket..\n");
-        return 1;
+        printf("Error creating socket!\n");
+        exit(1);
     }
-    printf("Server socket created..\n");
+    printf("Socket created...\n");
 
-    memset(&server_address, '\0', sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = PORT;
 
-    int b = bind(serverSocket, (struct sockaddr *)&server_address, serverLen);
-    if (b < 0)
+    ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret < 0)
     {
-        perror("Binding error..\n");
-        return 1;
+        printf("Error binding!\n");
+        exit(1);
     }
+    printf("Binding done...\n");
 
-    if (listen(serverSocket, 2) == 0)
+    printf("Waiting for a connection...\n");
+    listen(sockfd, 5);
+
+    for (;;)
     {
-        printf("Listening..\n");
-    }
-    else
-    {
-        perror("Can't listen to requests..\n");
-        return 1;
-    }
-    while (1)
-    {
-        newSocket = accept(serverSocket, (struct sockadddr *)&new_address, &clientLen);
-        if (newSocket < 0)
+        len = sizeof(cl_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *)&cl_addr, &len);
+        if (newsockfd < 0)
         {
-            perror("Can't accept requests..\n");
-            return 1;
+            printf("Error accepting connection!\n");
+            exit(1);
         }
-        time_stamp(currTime);
-        printf("%s\nConnection accepted from %d:%d\n", currTime, inet_ntoa(new_address.sin_addr), ntohs(new_address.sin_port));
+        printf("Connection accepted...\n");
+
+        inet_ntop(AF_INET, &(cl_addr.sin_addr), clientAddr, CLADDR_LEN);
         if ((childpid = fork()) == 0)
         {
-            close(serverSocket);
-            sprintf(buf, "Server says hello!");
-            send(newSocket, buf, strlen(buf), 0);
-            bzero(buf, sizeof(buf));
-            while (1)
+
+            close(sockfd);
+
+            for (;;)
             {
-                recv(newSocket, msg, sizeof(msg), 0);
-                if (strcmp(msg, "exit\n") == 0)
+                memset(buffer, 0, BUF_SIZE);
+                ret = recvfrom(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&cl_addr, &len);
+                if (ret < 0)
                 {
-                    time_stamp(currTime);
-                    printf("%s: Client %d disconnected from server..\n", currTime, inet_ntoa(new_address.sin_addr));
-                    return 1;
+                    printf("Error receiving data!\n");
+                    exit(1);
                 }
-                else
+                printf("Received data from %s: %s\n", clientAddr, buffer);
+
+                ret = sendto(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&cl_addr, len);
+                if (ret < 0)
                 {
-                    printf("Client %d: %s", ntohs(new_address.sin_port), msg);
-                    //sprintf(buf,"Client %d: %s\n",ntohs(new_address.sin_port),msg);
-                    printf("Enter Server: ");
-                    fgets(buf, SIZE - 1, stdin);
-                    //sprintf(buf, "Server: %s\n", msg);
-                    send(newSocket, buf, strlen(buf), 0);
-                    bzero(buf, sizeof(buf));
+                    printf("Error sending data!\n");
+                    exit(1);
                 }
+                printf("Sent data to %s: %s\n", clientAddr, buffer);
             }
         }
+        close(newsockfd);
     }
-    return 0;
 }
